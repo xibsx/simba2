c515e8c890e57b5762e3ea399569b096dcb4a02b
richbess22 <richbess22@gmail.com>
2026-01-06 02:18:18 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -39,7 +39,7 @@ const config = {
     GROUP_INVITE_LINK: 'https://chat.whatsapp.com/D3UTQublKxAAJfuqY366Iw',
     ADMIN_LIST_PATH: './admin.json',
     RCD_IMAGE_PATH: 'https://files.catbox.moe/jwmx1j.jpg',
-    NEWSLETTER_JID: '120363402325089913@newsletter',
+    NEWSLETTER_JID: '120363399470975987@newsletter',
     NEWSLETTER_MESSAGE_ID: '428',
     OTP_EXPIRY: 300000,
     version: '1.0.0',
